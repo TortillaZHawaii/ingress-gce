615bd079c89f26b7526ea9bf533e7d112d99972b
Dawid Wysocki <dwysocki@google.com>
2024-10-28 15:47:17 +0000

    L3 Backend

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1398,6 +1398,69 @@ func TestGetProtocol(t *testing.T) {
 	}
 }
 
+func TestGetBackendProtocol(t *testing.T) {
+	tcpPort := api_v1.ServicePort{
+		Name:     "TCP Port",
+		Protocol: api_v1.ProtocolTCP,
+	}
+	udpPort := api_v1.ServicePort{
+		Name:     "UDP Port",
+		Protocol: api_v1.ProtocolUDP,
+	}
+
+	testCases := []struct {
+		ports            []api_v1.ServicePort
+		expectedProtocol string
+		desc             string
+	}{
+		{
+			ports:            []api_v1.ServicePort{},
+			expectedProtocol: string(api_v1.ProtocolTCP),
+			desc:             "Empty ports should resolve to TCP",
+		},
+		{
+			ports:            []api_v1.ServicePort{
+				udpPort,
+			},
+			expectedProtocol: string(api_v1.ProtocolUDP),
+			desc:             "UDP protocol only",
+		},
+		{
+			ports:            []api_v1.ServicePort{
+				tcpPort,
+			},
+			expectedProtocol: string(api_v1.ProtocolTCP),
+			desc:             "TCP protocol only",
+		},
+		{
+			ports: []api_v1.ServicePort{
+				udpPort,
+				tcpPort,
+			},
+			expectedProtocol: "UNSPECIFIED",
+			desc:             "Mixed protocols, first UDP",
+		},
+		{
+			ports: []api_v1.ServicePort{
+				tcpPort,
+				udpPort,
+			},
+			expectedProtocol: "UNSPECIFIED",
+			desc:             "Mixed protocols, first TCP",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			protocol := GetBackendProtocol(tc.ports)
+
+			if protocol != tc.expectedProtocol {
+				t.Errorf("GetProtocol returned %v, not equal to expected protocol = %v", protocol, tc.expectedProtocol)
+			}
+		})
+	}
+}
+
 func TestGetPorts(t *testing.T) {
 	testCases := []struct {
 		ports         []api_v1.ServicePort
