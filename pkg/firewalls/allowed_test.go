1e68619daee6175b75f3ce7b33a4d3c98046ec4d
Dawid Wysocki <dwysocki@google.com>
2024-11-18 13:45:48 +0000

    Add firewalls.AllowedForService
    Adds function to get firewall allowed config for single or mixed protocol.

---
+++ pkg/firewalls/allowed_test.go
@@ -0,0 +1,106 @@
+package firewalls_test
+
+import (
+	"testing"
+
+	compute "google.golang.org/api/compute/v1"
+	api_v1 "k8s.io/api/core/v1"
+	"k8s.io/ingress-gce/pkg/firewalls"
+)
+
+func TestAllowedForService(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		svcPorts []api_v1.ServicePort
+		want     []*compute.FirewallAllowed
+	}{
+		{
+			desc: "empty",
+		},
+		{
+			desc: "udp only",
+			svcPorts: []api_v1.ServicePort{
+				{
+					Protocol: api_v1.ProtocolUDP,
+					Port:     1080,
+				},
+				{
+					Protocol: api_v1.ProtocolUDP,
+					Port:     8080,
+				},
+			},
+			want: []*compute.FirewallAllowed{
+				{
+					IPProtocol: "udp",
+					Ports:      []string{"1080", "8080"},
+				},
+			},
+		},
+		{
+			desc: "tcp only",
+			svcPorts: []api_v1.ServicePort{
+				{
+					Protocol: api_v1.ProtocolUDP,
+					Port:     1080,
+				},
+				{
+					Protocol: api_v1.ProtocolTCP,
+					Port:     80,
+				},
+				{
+					Protocol: api_v1.ProtocolTCP,
+					Port:     443,
+				},
+				{
+					Protocol: api_v1.ProtocolTCP,
+					Port:     8080,
+				},
+				{
+					Protocol: api_v1.ProtocolUDP,
+					Port:     8080,
+				},
+			},
+			want: []*compute.FirewallAllowed{
+				{
+					IPProtocol: "udp",
+					Ports:      []string{"1080", "8080"},
+				},
+				{
+					IPProtocol: "tcp",
+					Ports:      []string{"80", "443", "8080"},
+				},
+			},
+		},
+		{
+			desc: "mixed protocol",
+			svcPorts: []api_v1.ServicePort{
+				{
+					Protocol: api_v1.ProtocolTCP,
+					Port:     80,
+				},
+				{
+					Protocol: api_v1.ProtocolTCP,
+					Port:     443,
+				},
+				{
+					Protocol: api_v1.ProtocolTCP,
+					Port:     8080,
+				},
+			},
+			want: []*compute.FirewallAllowed{
+				{
+					IPProtocol: "tcp",
+					Ports:      []string{"80", "443", "8080"},
+				},
+			},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			got := firewalls.AllowedForService(tC.svcPorts)
+			if eq, err := firewalls.EqualAllowRules(got, tC.want); !eq || err != nil {
+				t.Errorf("AllowedForService(_) = %v, want %v", got, tC.want)
+			}
+		})
+	}
+}
